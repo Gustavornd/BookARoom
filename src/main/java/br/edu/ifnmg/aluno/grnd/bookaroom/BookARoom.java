10ee50b099b7bd7317fe2a3f6968815bb4b4ab72
Gustavo Rafael Nunes Duraes <gustavo.ipatinga@gmail.com>
2023-03-15 21:55:54 -0300

    Primeiro Commit

---
+++ src/main/java/br/edu/ifnmg/aluno/grnd/bookaroom/BookARoom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifnmg.aluno.grnd.bookaroom;
+
+/**
+ *
+ * @author gusta
+ */
+public class BookARoom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
